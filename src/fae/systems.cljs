3b9bc1e0342ac9a282e3ae57f75fe7bdee08f074
Ben Follington <5009316+bfollington@users.noreply.github.com>
2021-03-09 09:59:17 +1000

    Fully empty event queue
    This will stop spreading events dispatched during update over multiple frames

--- src/fae/systems.cljs
+++ src/fae/systems.cljs
@@ -1,6 +1,7 @@
 (ns fae.systems
   (:require [fae.engine :as engine]
             [fae.events :as events]
+            [fae.util :as util]
             [fae.print :as print]))
 
 (defn update-system [node state]
@@ -40,17 +41,34 @@
 (def state [update-system rotate-system])
 (def effect [graphics-system])
 
+(def BAILOUT 100)
+
 (defn execute-events [state hook-fn]
-  (let [events @events/inbox]
-    ;; (when (> (count events) 0)
-    ;;   (println "processing events" events))
+  ;; don't be scared, this is a little gnarly looking
+  ;; in our game processing events can yield MORE events
+  ;; so after processing the queue, check if it has refilled
+  ;; if it had, process again
+  ;;   
+  ;; keep going until either the queue is empty or BAILOUT attempts occur
+
+  (loop [events @events/inbox
+         counter 0]
+    (when (> (count events) 0)
+      (println (util/format "pass %i: processing events" counter) events))
+
     (events/clear-inbox!)
     (let [state' (update state :actors
                          (fn [actors]
                            (map (fn [node] (event-processing-system node state events))
                                 actors)))
           state'' (reduce (fn [s [ev data]] (hook-fn s ev data)) state' events)]
-      state'')))
+      (if (and (> (count @events/inbox) 0)
+               (< counter BAILOUT))
+        (recur @events/inbox (inc counter))
+        (do
+          (when (>= counter BAILOUT)
+            (js/console.error (util/format "Event queue failed to empty after %i attempts" BAILOUT)))
+          state'')))))
 
 (defn execute-state [state system]
   (update state :actors
